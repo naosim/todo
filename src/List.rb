4fe01b2b0034a285ca186d61013fc1bc95245600
naosim <nao_pillows@yahoo.co.jp>
2013-08-03 12:06:52 +0900

    終わったやつ、終わってないやつを分けて出す機能追加

--- src/List.rb
+++ src/List.rb
@@ -1,14 +1,34 @@
 require 'src/Executor'
+
 class List < Executor
   def run(tasks)
+    if !isKey?(@argv[0]) || @argv.length == 1
+      showAll(tasks)
+    elsif @argv[1] == "-s"
+      showWithFilter(tasks, true);
+      showWithFilter(tasks, false);
+    end
+  end
+
+  def showAll(tasks)
     i = 0;
+
     tasks.each{|task|
       finished = task.isFinished? ? 'v' : ' ';
       outputer.output("#{finished} #{i}.#{task.name}\n")
       i += 1
     }
   end
-  
+
+  def showWithFilter(tasks, isDone)
+    i = 0;
+    tasks.each{|task|
+      finished = task.isFinished? ? 'v' : ' ';
+      outputer.output("#{finished} #{i}.#{task.name}\n") if task.isFinished? == isDone
+      i += 1
+    }
+  end
+
   def isKey?(key)
     return key == "ls"
   end
